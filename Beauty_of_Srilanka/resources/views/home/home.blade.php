36ef0855287b198762d5ff4a673d3a0dd441e5cf
Mishan Madhupa <mishancool2016@gmail.com>
2019-12-30 21:24:39 +0530

    Style home page

--- Beauty_of_Srilanka/resources/views/home/home.blade.php
+++ Beauty_of_Srilanka/resources/views/home/home.blade.php
@@ -57,27 +57,27 @@
     <!-- END nav -->
 
     <section class="home-slider owl-carousel">
-        <div class="slider-item" style="background-image:url(images/bg_1.jpg);">
+        <div class="slider-item" style="background-image:url(images/home1.jpg);">
             <div class="overlay"></div>
           <div class="container">
             <div class="row no-gutters slider-text align-items-center justify-content-start" data-scrollax-parent="true">
             <div class="col-md-6 ftco-animate">
-              <h1 class="mb-4">Education Needs Complete Solution</h1>
+              <h1 class="mb-4">Beauty of Sri Lanka</h1>
               <p>A small river named Duden flows by their place and supplies it with the necessary regelialia.</p>
-              <p><a href="#" class="btn btn-primary px-4 py-3 mt-3">Contact Us</a></p>
+              {{-- <a><a href="#" class="btn btn-primary px-4 py-3 mt-3">Contact Us</a></a> --}}
             </div>
           </div>
           </div>
         </div>
 
-        <div class="slider-item" style="background-image:url(images/bg_2.jpg);">
+        <div class="slider-item" style="background-image:url(images/home2.jpg);">
             <div class="overlay"></div>
           <div class="container">
             <div class="row no-gutters slider-text align-items-center justify-content-start" data-scrollax-parent="true">
             <div class="col-md-6 ftco-animate">
-              <h1 class="mb-4">University, College School Education</h1>
+              <h1 class="mb-4">Protect Sri Lanka</h1>
               <p>A small river named Duden flows by their place and supplies it with the necessary regelialia.</p>
-              <p><a href="#" class="btn btn-primary px-4 py-3 mt-3">Contact Us</a></p>
+              {{-- <a><a href="#" class="btn btn-primary px-4 py-3 mt-3">Contact Us</a></a> --}}
             </div>
           </div>
           </div>
